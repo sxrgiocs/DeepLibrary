5828958cb55d29c3e6eaa3eac406d613e131596c
sxrgiocs <scarrerasal@gmail.com>
2023-06-19 10:57:55 +0200

    Changed 3D U-Net

---
+++ PyTorch/Segmentation/Models/UNet/model.py
@@ -0,0 +1,139 @@
+import torch.nn as nn
+from modules import DoubleConv, Down, Up, Out
+
+
+class UNet(nn.Module):
+    """
+    3D U-Net model for semantic segmentation. Based on the original paper by
+    Ronneberger et al. https://arxiv.org/abs/1505.04597. Currently works for 3D
+    images. The network is composed by 4 encoder filters which encode the
+    input image, one bottleneck layer to reduce dimensionality and 4 decoder
+    layers to produce the segmentation.
+
+    Parameters
+    ----------
+    in_channels : int
+        Number of input channels. Works for both RGB and grayscale images.
+    n_classes : int
+        Number of output classes. Refers number of labels in the segmentation
+        plus the background (eg. 4 labels + background -> nclasses = 5).
+    filter_factor : float
+        Factor to adjust the number of filters in each layer. Defaults to 1,
+        keeping the same number of filters as in the paper by Ronneberger.
+
+    Attributes
+    ----------
+    encoder : torch.nn.Sequential
+        Encoder path of the U-Net.
+    bottleneck : DoubleConv
+        Bottleneck layer of the U-Net.
+    decoder : nn.Sequential
+        Decoder path of the U-Net.
+    out : Out
+        Output layer of the U-Net.
+    """
+
+    def __init__(self, in_channels, n_classes, filter_factor=1):
+        super(UNet, self).__init__()
+
+        filters = [64, 128, 256, 512]
+        filters = [int(f * filter_factor) for f in filters]
+
+        self.encoder = self._create_encoder(in_channels, filters)
+        self.bottleneck = DoubleConv(filters[-1], filters[-1] * 2)
+        self.decoder = self._create_decoder(filters)
+        self.out = Out(filters[0], n_classes)
+
+    def forward(self, x):
+        """
+        Forward pass of the U-Net model.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            Input tensor.
+
+        Returns
+        -------
+        out : torch.Tensor
+            Output tensor representing the segmentation prediction. It is
+            a probabilistic segmentation since Softmax function is applied.
+
+        """
+
+        # Create an empty list to store the encoders for the skip-connections
+        encoders = []
+        for encoder in self.encoder:
+            x = encoder(x)
+            encoders.append(x)
+
+        x = self.bottleneck(x)
+
+        for i, decoder in enumerate(self.decoder):
+            x = decoder(x, encoders[-(i + 1)])
+
+        out = self.out(x)
+
+        return out
+
+    def _create_encoder(self, in_channels, filters):
+        """
+        Function uses to create the encoder path of the U-Net. It takes a list
+        containing the number of features as an input argument, iterates
+        through it and appends each encoder layer to an empty list. Then, the
+        list is used to create a torch.nn.Sequential object that can be used in
+        the forward pass.
+
+        Parameters
+        ----------
+        in_channels : int
+            Number of input channels.
+        filters : list[int]
+            List of number of filters for each encoder layer.
+
+        Returns
+        -------
+        encoder : nn.Sequential
+            Encoder path of the U-Net.
+        """
+
+        # Create empty list to store each encoder layer
+        encoder_layers = []
+        # First decoder layer takes the input channels, so create it separately
+        encoder_layers.append(DoubleConv(in_channels, filters[0]))
+        # Create the rest of the layers using a for loop
+        for i in range(len(filters) - 1):
+            encoder_layers.append(Down(filters[i], filters[i + 1]))
+
+        encoder = nn.Sequential(*encoder_layers)
+
+        return encoder
+
+    def _create_decoder(self, filters):
+        """
+        Function uses to create the decoder path of the U-Net. It takes a list
+        containing the number of features as an input argument, iterates
+        through it and appends each decoder layer to an empty list. Then, the
+        list is used to create a torch.nn.Sequential object that can be used in
+        the forward pass.
+
+        Parameters
+        ----------
+        filters : list[int]
+            List of number of filters for each encoder layer.
+
+        Returns
+        -------
+        decoder : nn.Sequential
+            Decoder path of the U-Net.
+        """
+
+        # Create empty list to store each decoder layer
+        decoder_layers = []
+        # Create the layers using a for loop
+        for i in range(len(filters) - 1, -1, -1):
+            decoder_layers.append(Up(filters[i] * 2, filters[i]))
+
+        decoder = nn.Sequential(*decoder_layers)
+
+        return decoder
